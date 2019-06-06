ca4692b11e0aaa6173f68c978b1ecc7c879d9c47
David Sabalete <dsabalete@gmail.com>
2019-06-06 21:04:37 +0200

    Create Burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import classes from './burgerIngredient.module.css'
+import classes from './BurgerIngredient.module.css'
 
-const burgerIngredient = props => {
+const BurgerIngredient = props => {
     let ingredient = null
 
     switch (props.type) {
@@ -36,8 +36,8 @@ const burgerIngredient = props => {
     return ingredient
 }
 
-burgerIngredient.propType = {
+BurgerIngredient.propType = {
     type: PropTypes.string.isRequired
 }
 
-export default burgerIngredient
+export default BurgerIngredient
